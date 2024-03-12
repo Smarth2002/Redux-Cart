480b97d243b679000aaa51e96c4f9e9ff3027bf5
Smarth2002 <smarthkumar789@gmail.com>
2024-03-12 19:24:26 +0530

    h

--- src/index.js
+++ src/index.js
@@ -5,23 +5,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCBhgOAQf8u-ZwO7RXSYPiMAl7vsR-gzWI",
-//   authDomain: "food-order-app-d2989.firebaseapp.com",
-//   projectId: "food-order-app-d2989",
-//   storageBucket: "food-order-app-d2989.appspot.com",
-//   messagingSenderId: "764692383750",
-//   appId: "1:764692383750:web:d2a1a7fdeb42cd46295b87"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
